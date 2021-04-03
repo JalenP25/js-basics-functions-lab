b1145c7c5e4cd4cf29dc220e8034bf1b8cfd4d00
Jalen Peeler <jalenpeeler@yahoo.com>
2021-04-03 03:23:37 -0400

    Done functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+    if (blocks >= 43) {
+        return blocks - 42;
+    } else {
+        return 42 - blocks;
+    }
+};
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks (blocks)*264;
+};
+
+function distanceTravelledInFeet(beg, dest) {
+    if (dest > beg) {
+    return (dest - beg)*264;
+ } else {
+     return (beg - dest)*264;
+ }
+}
+
+function calculatesFarePrice(beg, dest) {
+    const distance = distanceTravelledInFeet(beg, dest);
+    if (distance <= 400) {
+        return 0;
+    } else if (400 < distance && distance < 2000) {
+        return 2.56;
+    } else if (2000 < distance && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
